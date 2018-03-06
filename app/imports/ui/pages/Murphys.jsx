94125a54ff30dc59a14292545767383c45b6fea0
rcammack <rcammack@hawaii.edu>
2018-03-06 11:27:46 -1000

    Completed Murphys mockup

---
+++ app/imports/ui/pages/Murphys.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Middle from '../components/Middle';
+
+export default class Murphys extends React.Component {
+  render() {
+    return (
+        <div>
+          <Middle/>
+        </div>
+    );
+  }
+}
